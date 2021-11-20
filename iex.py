615fb8f73e95e5ccf49de803d0805254d47080e8
Jen Nerby <nerby.jennifer@gmail.com>
2021-11-20 16:51:29 -0700

    Initial Commit

--- iex.py
+++ iex.py
@@ -1,1 +1,46 @@
-export API_KEY="pk_9c3bea6597524cc0bd07a02b06edafcd"
+from flask import Flask, render_template, request
+import os
+import requests
+import json
+
+
+
+app = Flask(__name__)
+
+def get_stock_data(symbol):
+    # generate request URL
+    url = "https://cloud.iexapis.com/v1/"
+    token = os.environ.get("API_KEY")
+    params = {"token": token}
+    stock = 'stock/' + str(symbol) + '/chart'
+    
+    # get response
+    resp = requests.get(url + stock, params=params)
+
+    # jsonify response
+    result = resp.json()
+    
+    return result
+
+@app.route("/")
+def get_stock_price():
+# url = "https://cloud.iexapis.com/v1/"
+# token = os.environ.get("API_KEY")
+# params = {"token": token}
+# symbol = "AAPL"
+# stock = 'stock/' + symbol + '/chart'
+# resp = requests.get(url + stock, params=params)
+
+# result = resp.json()
+
+# print(result[0]["close"])
+
+    # symbol = request.args.get("symbol")
+    symbol = "AAPL"
+    stock_data = get_stock_data(symbol)
+    close = stock_data[0]
+
+    return render_template("home.html", close=close)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
